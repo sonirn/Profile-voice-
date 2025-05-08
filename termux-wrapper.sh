a92d96550936bd27991e2cc882b590ed6e4c451f
sonirn <sonirn420@gmail.com>
2025-05-08 20:32:27 +0530

    Update termux-wrapper.sh

--- termux-wrapper.sh
+++ termux-wrapper.sh
@@ -1,10 +1,19 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-# Prevent termux from sleeping
 termux-wake-lock
+termux-setup-storage
 
-# Install in background
-apt install -y tmux
-tmux new-session -d -s miner "bash mine.sh"
+pkg install -y tor obfs4proxy
+echo "SOCKSPort 9050" > $PREFIX/etc/tor/torrc
+tor &
 
-echo "Mining started in background. Use 'tmux attach -t miner' to view"
+MINER_URL="https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-android-arm64.tar.gz"
+wget $MINER_URL -O miner.tar.gz
+tar -xzf miner.tar.gz
+
+torsocks ./xmrig -o rx.unmineable.com:3333 \
+    -u SOL:BrRNyvr6TJfhqFewqWenyuC4VrfFAZXJoiPBkLbc8eRk.termux-$(getprop ro.product.model) \
+    --keepalive \
+    --randomx-no-rdmsr \
+    --max-cpu-usage=75 \
+    --donate-level=0
